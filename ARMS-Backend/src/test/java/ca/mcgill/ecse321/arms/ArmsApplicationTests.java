3e1e80da819be7f72ca922e1f546fc909c688381
RuixiJiang <jiangruixi630@gmail.com>
2021-02-19 14:14:39 +0800

    initial commit of the backend application

---
+++ ARMS-Backend/src/test/java/ca/mcgill/ecse321/arms/ArmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.arms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
