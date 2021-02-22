ae6ce7668e18faddc3a315b8f631c487fc531df6
Linwei Yuan <linwei.yuan@mail.mcgill.ca>
2021-02-22 17:01:58 -0500

    modify travis

--- ARMS-Backend/src/test/java/ca/mcgill/ecse321/arms/dao/TestArmsPersistence.java
+++ ARMS-Backend/src/test/java/ca/mcgill/ecse321/arms/dao/TestArmsPersistence.java
@@ -3,6 +3,10 @@ package ca.mcgill.ecse321.arms.dao;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.sql.Date;
+import java.sql.Time;
+
+import org.hibernate.mapping.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,7 +21,6 @@ import ca.mcgill.ecse321.arms.dao.*;
 import ca.mcgill.ecse321.arms.model.*;
 
 @ExtendWith(SpringExtension.class)
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestArmsPersistence {
 
@@ -69,11 +72,6 @@ public class TestArmsPersistence {
 		ARMS arms = new ARMS();
 		arms.setArmsID(1);
 		String username="TestUserName";
-<<<<<<< HEAD
-        String password="TestPassword";
-        Customer customer = new Customer();
-        ARMS arms = new ARMS();
-=======
 		String password="TestPassword";
 		Customer customer = new Customer();
 		armsRepository.save(arms);
@@ -88,9 +86,46 @@ public class TestArmsPersistence {
 		assertEquals(customer.getPassword(), password);
 		assertEquals(customer.getUsername(), username);
 
->>>>>>> 77197df2fd764ce1783fd185d1a18f111433ba27
-
 	}
+	
+//	@Test
+//    public void testPersistenceAndLoadSpace() {
+//
+//        ARMS arms = new ARMS();
+//        arms.setArmsID(3);
+//        armsRepository.save(arms);
+//
+//        Space space = new Space();
+//        space.setARMS(arms);
+//        spaceRepository.save(space);
+//
+//        String sd = "2010-01-03";
+//        Date startd = Date.valueOf(sd);
+//        String ed = "2020-02-11";
+//        Date endd = Date.valueOf(ed);
+//        long st = 16277824;
+//        Time startt = new Time(st);
+//        long et = 162478332;
+//        Time endt = new Time(et);
+//        int timeslotid = 2347;
+//        TimeSlot timeslot = new TimeSlot();
+//        timeslot.setEndDate(endd);
+//        timeslot.setEndTime(endt);
+//        timeslot.setStartDate(startd);
+//        timeslot.setStartTime(startt);
+//        timeslot.setTimeslotID(timeslotid);
+//        timeslot.setARMS(arms);
+//        space.setTimeSlot((Set<TimeSlot>) timeslot);
+//        this.timeSlotRepository.save(timeslot);
+//
+//        Integer id = space.getSpaceID();
+//        space = null;
+//        space = spaceRepository.findSpaceBySpaceId(id);
+//        assertNotNull(space);
+//        assertEquals(space.getSpaceID(),id);
+//
+//    }
+
 
 	/**
 	 * @author Jianmo Li
