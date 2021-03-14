ff0b05177570854684d77d0fc06d7979a9541db7
Will-Li-zw <55262811+Will-Li-zw@users.noreply.github.com>
2021-03-14 20:36:31 +0800

    Revert "Zhiwei"

---
+++ ARMS-Backend/src/main/java/ca/mcgill/ecse321/arms/controller/TechnicianController.java
@@ -0,0 +1,65 @@
+package ca.mcgill.ecse321.arms.controller;
+
+import ca.mcgill.ecse321.arms.dao.TechnicianRepository;
+import ca.mcgill.ecse321.arms.dto.TechnicianDto;
+import ca.mcgill.ecse321.arms.model.Technician;
+import ca.mcgill.ecse321.arms.service.TechnicianService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class TechnicianController {
+    @Autowired
+    private TechnicianService technicianService;
+    @Autowired
+    private TechnicianRepository technicianRepository;
+
+    @PostMapping(value = {"/technician", "/technician/"})
+    public TechnicianDto createTechnician(
+            @RequestParam("id") int id,
+            @RequestParam("name") String name,
+            @RequestParam("email") String email
+    ){
+        Technician technician = technicianService.createTechnician(name, email, id);
+        return convertToDto(technician);
+    }
+
+    @PutMapping(value = {"/updateTechnician", "/updateTechnician/"})
+    public TechnicianDto updateTechnician(
+            @RequestParam("id") int id,
+            @RequestParam("name") String name,
+            @RequestParam("email") String email
+    ){
+        Technician technician = technicianService.updateTechnician(id, name, email);
+        return convertToDto(technician);
+    }
+
+    @GetMapping(value = {"/technicians", "/technicians/"})
+    public List<TechnicianDto> getAllTechnicians(){
+        List<TechnicianDto> technicianDtos = new ArrayList<>();
+        for(Technician technician: technicianService.getAllTechnicians()){
+            technicianDtos.add(convertToDto(technician));
+        }
+        return getAllTechnicians();
+    }
+
+    @DeleteMapping(value = {"/deleteTechnician", "/deleteTechnician/"})
+    public void deleteTechnician(
+            @RequestParam("id") int id
+    ){
+        technicianService.deleteTechnician(id);
+    }
+
+    public TechnicianDto convertToDto(Technician technician){
+        if(technician==null){
+            throw new IllegalArgumentException("There is no such technician");
+        }
+        TechnicianDto technicianDto = new TechnicianDto(technician.getName(), technician.getEmail(), technician.getTechnicianID());
+        return technicianDto;
+    }
+
+}
