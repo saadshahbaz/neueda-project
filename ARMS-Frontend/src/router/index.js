e06d12b4053f8322200ff26c939e3d0987b959b0
LiJianmo <jianmo.li@mail.mcgill.ca>
2021-03-30 23:26:53 +0800

    sss

--- ARMS-Frontend/src/router/index.js
+++ ARMS-Frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import dashboard from '../views/Dashboard'
 import landingPage from '../components/LandingPage'
+import customerDashboard from "../views/customerDashboard";
 
 Vue.use(VueRouter)
 
@@ -42,34 +43,30 @@ const routes = [
     ]
   },
   {
-    path: '/home',
-    name: 'home',
-    component: dashboard,
+    path: '/customerHome',
+    name: 'customerHome',
+    component: customerDashboard,
     children: [
       {
         path: '',
-        component: () => import(/* webpackChunkName: "Overview" */ '../views/Business.vue')
+        component: () => import(/* webpackChunkName: "Overview" */ '../views/Profile.vue')
       },
       {
-        path: 'services',
-        component: () => import(/* webpackChunkName: "Messages" */ '../views/Services.vue')
+        path: 'car',
+        component: () => import(/* webpackChunkName: "Profile" */ '../views/Car.vue')
       },
       {
-        path: 'space',
-        component: () => import(/* webpackChunkName: "Profile" */ '../views/Space.vue')
+        path: 'customerAppointment',
+        component: () => import(/* webpackChunkName: "Messages" */ '../views/customerAppointment.vue')
       },
       {
-        path: 'technicians',
-        component: () => import(/* webpackChunkName: "Settings" */ '../views/Technicians.vue')
+        path: 'bill',
+        component: () => import(/* webpackChunkName: "Settings" */ '../views/Bill.vue')
       },
       {
-        path: 'appointments',
-        component: () => import(/* webpackChunkName: "Settings" */ '../views/Appointments.vue')
+        path: 'deleteAccount',
+        component: () => import(/* webpackChunkName: "Settings" */ '../views/DeleteAccount.vue')
       },
-      {
-        path: 'reminders',
-        component: () => import(/* webpackChunkName: "Settings" */ '../views/Reminders.vue')
-      }
     ]
   }
 ]
