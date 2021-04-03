6fcbe3e7a8df19c2d5fa36b62efdd7b7fa84e8c0
Will-Li-zw <zhiwei.li2@mail.mcgill.ca>
2021-04-03 10:55:57 +0800

    add deletion

--- ARMS-Frontend/src/views/DeleteAccount.vue
+++ ARMS-Frontend/src/views/DeleteAccount.vue
@@ -1,10 +1,39 @@
 <template>
-
+  <div class="row my-3 px-3"><input
+    name="Delete"
+    id="OldPassword"
+    placeholder="Please input delete to confirm"
+    v-model="deleteAccount"/></div>
+  <div class="button px-3 py-1 mt-1 mb-3" type="submit" @click="deleteAccount">Delete Account</div>
 </template>
 
 <script>
+import {AXIOS} from "../components/axiosInstance";
 export default {
-  name: "DeleteAccount"
+  name: "DeleteAccount",
+  data(){
+    return{
+      deleteAccount : "",
+    };
+  },
+  methods: {
+  deleteAccount: function () {
+    if (this.deleteAccount!= "delete") {
+      alert("Please enter delete to confrim");
+    }  else {
+      AXIOS.delete(`/deleteCustomer`)
+        .then((response) => {
+        this.deleteAccount="";
+          alert("Delete Successfully");
+      })
+        .catch((e) => {
+          alert("You have unpaid bills");
+        });
+
+    }
+
+    }
+  },
 }
 </script>
 
