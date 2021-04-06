5562b33ea7208a119d01110033a869ea70278a04
RuixiJiang <jiangruixi630@gmail.com>
2021-04-06 10:01:10 +0800

    minor fix

--- ARMS-Backend/src/main/java/ca/mcgill/ecse321/arms/dao/BillRepository.java
+++ ARMS-Backend/src/main/java/ca/mcgill/ecse321/arms/dao/BillRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface BillRepository extends CrudRepository<Bill, String> {
     Bill findBillByBillNo(int billNo);
     List<Bill> findBillsByCustomer(Customer customer);
+    Integer deleteByBillNo(int billNo);
 }
